a632d1b89d64b3eedd78915e5e6833351eb1299c
Andrew <moon1ight2013@yandex.ru>
2021-11-09 17:55:09 +0300

    Работа со строками

---
+++ string.js
@@ -0,0 +1,21 @@
+function showVerticalMessage(string) {
+
+    if (string[0] == 'м') {
+        string = 'M' + string.slice(1);
+    }
+
+    if (string.length > 10) {
+        for (let i = 0; i < 10; i++) {
+            console.log(string[i]) + '/n';
+        }
+    }
+    else {
+        for (let i = 0; i < string.length; i++) {
+            console.log(string[i]) + '/n';
+        }
+
+    }
+
+}
+showVerticalMessage('марафон');
+showVerticalMessage('Не марафон, а марафонище');
---
+++ test.js
@@ -0,0 +1,31 @@
+/* let user = {
+    name: "Иван",
+    age: 30
+};
+
+let clone = {}; // новый пустой объект
+
+for (let key in user) {
+    clone[key] = user[key];
+}
+
+console.log(clone.age);
+
+let clone1 = {};
+Object.assign(clone1, user);
+
+
+console.log(user.age); */
+
+/* let user = {
+    name: 'John',
+    age: 30
+}
+
+user.sayHi = function () {
+    alert("Привет!");
+};
+
+console.log(user.sayHi);
+console.log(user.sayHi());
+ */
