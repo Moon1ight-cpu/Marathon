f46c26090a371f1f492376a99617e1084ff584f0
Andrew <moon1ight2013@yandex.ru>
2021-11-09 18:14:54 +0300

    Рефакторинг строк 2

--- string.js
+++ string.js
@@ -6,12 +6,12 @@ function showVerticalMessage(string) {
 
     if (string.length > 10) {
         for (let i = 0; i < 10; i++) {
-            console.log(string[i]) + '/n';
+            console.log(string[i]);
         }
         return;
     }
     for (let i = 0; i < string.length; i++) {
-        console.log(string[i]) + '/n';
+        console.log(string[i]);
     }
 }
 showVerticalMessage('марафон');
