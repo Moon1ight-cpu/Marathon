98b59051d84d9a77fb161a8881ccb643fec1cbbe
Andrew <moon1ight2013@yandex.ru>
2021-12-19 01:46:35 +0300

    я танцую макарэну

--- Weather/js/main.js
+++ Weather/js/main.js
@@ -6,6 +6,7 @@ const UI = {
     FAVORITES: document.querySelector('.favorites'),
     CLOSE_BUTTONS: document.querySelectorAll('.locations ul li img'),
     LIKE_BUTTON: document.querySelector('.like img'),
+    BOOKMARKS: document.querySelectorAll('.favorites-city span'),
 }
 const URL = {
     SERVER: 'http://api.openweathermap.org/data/2.5/weather',
@@ -13,40 +14,53 @@ const URL = {
 }
 const FAVORITES_CITIES = ['Amur', 'Samara', 'Bali', 'Dane', 'Kilo', 'Nur-Sultan'];
 
-/* ------------------------------------ОБРАБОТЧИК САБМИТА----------------------------------------------------- */
 
-UI.FORM.addEventListener('submit', function () {
-    const CITY_NAME = UI.INPUT.value;
-    const url = `${URL.SERVER}?q=${CITY_NAME}&appid=${URL.API_KEY}`;
 
+function showWeather(cityName) {
+    const url = `${URL.SERVER}?q=${cityName}&appid=${URL.API_KEY}`;
     fetch(url)
         .then(response => response.json())
         .then(answer => {
-            console.log(answer);
+            if (answer.cod === '404') {
+                alert(answer.message);
+            }
             UI.NOW_CITY.textContent = answer.name;
             UI.NOW_TEMPERATURE.textContent = Math.round(answer.main.temp - 273) + '°';
-        });
-
+        })
+        .catch(error => alert(error.message));
     UI.FORM.reset();
+}
+
+UI.FORM.addEventListener('submit', function () {
+    showWeather(UI.INPUT.value);
 });
 
-/* ------------------------------------УДАЛЕНИЕ ГОРОДА----------------------------------------------------- */
+
 function deleteFavoriteCity() {
     this.parentElement.remove();
     FAVORITES_CITIES.splice(FAVORITES_CITIES.indexOf(this.previousElementSibling.textContent), 1);
 }
+
 UI.CLOSE_BUTTONS.forEach(function (item) {
     item.addEventListener('click', deleteFavoriteCity);
 })
+
 UI.LIKE_BUTTON.addEventListener('click', function () {
     if (!FAVORITES_CITIES.includes(UI.NOW_CITY.textContent)) {
         const LI = document.createElement('li');
         LI.className = 'favorites-city';
         LI.innerHTML = `<span>${UI.NOW_CITY.textContent}</span>
     <img src="img/close_thick_icon_137749.svg" width="20px" height="20px">`
-        LI.querySelector('img').addEventListener('click', deleteFavoriteCity)
+        LI.querySelector('img').addEventListener('click', deleteFavoriteCity);
+        LI.querySelector('span').addEventListener('click', bookmarksCityClick);
         UI.FAVORITES.append(LI);
         FAVORITES_CITIES.push(UI.NOW_CITY.textContent);
     }
 });
 
+function bookmarksCityClick() {
+    showWeather(this.textContent);
+}
+UI.BOOKMARKS.forEach(function (item) {
+    item.addEventListener('click', bookmarksCityClick);
+})
